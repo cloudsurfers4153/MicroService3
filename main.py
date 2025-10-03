a38a16ac0e65cf9e53e2ac0c92a97b0c7bc70f66
wd2002 <wangdi_wd2002@163.com>
2025-10-03 16:08:13 -0400

    change the ip

--- main.py
+++ main.py
@@ -64,4 +64,4 @@ def root():
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run("main:app", host="127.0.0.1", port=port, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
